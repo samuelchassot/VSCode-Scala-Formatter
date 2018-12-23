9670a5e942cd4e9e5453fbd7c2f7c7413bde9fa7
samuelchassot <samuel.chassot@epfl.ch>
2018-12-23 20:00:54 +0100

    add Main object and LanguageServer and trying to run ut

--- src/ch/chassot/vsscalafmt/ScalaLanguageServer.scala
+++ src/ch/chassot/vsscalafmt/ScalaLanguageServer.scala
@@ -7,6 +7,8 @@ import org.scalafmt
 import org.eclipse.lsp4j
 import org.eclipse.lsp4j.{DocumentFormattingParams, TextEdit}
 import org.eclipse.lsp4j.services._
+import org.scalafmt.config.ScalafmtConfig
+import scala.meta.Dialect
 
 
 /** An implementation of the Language Server Protocol for Dotty that responds to format requests.
@@ -19,6 +21,22 @@ class ScalaLanguageServer extends LanguageServer with TextDocumentService {
 
 
   override def formatting(params: DocumentFormattingParams): CompletableFuture[util.List[_ <: TextEdit]] = {
-    super.formatting(params)
+
+    val source = scala.io.Source.fromFile(params.getTextDocument.getUri)
+    val code = try source.mkString finally source.close()
+
+    val future = new CompletableFuture[util.List[_ <: TextEdit]]()
+    future.complete(formatDottyCode(code))
+
+    def formatDottyCode(code: String): List[_ <: TextEdit] = {
+      val config: ScalafmtConfig = ScalafmtConfig.default.withDialect(Dialect.Dotty)
+      )
+      scalafmt.Scalafmt.format(
+        code = code,
+        style = config
+      )
+    }
+
+
   }
 }
